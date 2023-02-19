4e9839060adb7e2eb7916c9c6461af3fff6bacf5
Canioni-S <stephan.canioni@outlook.fr>
2023-02-19 09:31:46 +0100

    page load speed up and seo update

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ if (!empty($_POST)) {
 
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="Portfolio de Stéphan Canioni développeur web">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
@@ -48,27 +49,26 @@ if (!empty($_POST)) {
             <div id="linksContainer" class="navBarBurger">
                 <a href="#presentationSection" class="navLink flexCenter">
                     <p class="textLink">présentation</p>
-                    <img src="../Assest/NavbarImg/presentationLinkImg.webp" alt="Photo de présentation, deux hommes se serrent la main" class="navLinkImg">
+                    <img src="../Assest/NavbarImg/presentationLinkImg.webp" alt="Photo de présentation, deux hommes se serrent la main." class="navLinkImg">
                 </a>
                 <a href="#workSection" class="navLink flexCenter" id="workLink">
                     <p class="textLink">réalisation</p>
-                    <img src="../Assest/NavbarImg/workLinkImg.webp" alt="Photo mettant en scène un homme de dos travaillant dans le codage informatique sur un ordinateur" class="navLinkImg">
+                    <img src="../Assest/NavbarImg/workLinkImg.webp" alt="Photo mettant en scène un homme de dos travaillant dans le codage informatique sur un ordinateur." class="navLinkImg">
                 </a>
                 <a href="#contactSection" class="navLink flexCenter" id="contactLink">
                     <p class="textLink">contact</p>
-                    <img src="../Assest/NavbarImg/contactLinkImg.webp" alt="Photo d'un homme tennant un téléphone affichant un contact" class="navLinkImg">
+                    <img src="../Assest/NavbarImg/contactLinkImg.webp" alt="Photo d'un homme tenant un téléphone affichant un contact." class="navLinkImg">
                 </a>
                 <a href="/Assest/Canioni-Stéphan-CV.pdf" class="navLink flexCenter" id="cvLink" download="Canioni-Stéphan-CV" onclick="return confirm('Confirm download Canioni-Stéphan-CV?');">
                     <p class="textLink">télécharger cv</p>
-                    <img src="../Assest/NavbarImg/cvLinkImg.webp" alt="Photo d'un curriculum vitae en papier sur une table" class="navLinkImg">
+                    <img src="../Assest/NavbarImg/cvLinkImg.webp" alt="Photo d'un curriculum vitae en papier sur une table." class="navLinkImg">
                 </a>
             </div>
         </nav>
 
         <!-- HOME SECTION -->
         <div id="homeSection">
-            <!-- <img id="homeBG" src="/Assest/HomeSectionImg/space-galaxy-background.jpg" alt="Background of the milky way in a dark galaxy"> -->
-            <img id="homeBG" src="/Assest/HomeSectionImg/milky-way-galaxy-night.jpg" alt="Background of the milky way in a dark galaxy">
+            <img id="homeBG" src="/Assest/HomeSectionImg/milky-way-galaxy-night-W=1900.webp" alt="Arrière-plan d'image représentant la voie lactée.">
             <div id="fullNameContainer">
                 <div class="fullName">
                     <div id="firstName">
@@ -139,14 +139,14 @@ if (!empty($_POST)) {
             </div>
 
             <div id="scrollIconContainer" class="flexCenter">
-                <svg id="fingerScroll" version="1.0" xmlns="http://www.w3.org/2000/svg" width="50%" height="auto" viewBox="0 0 30.000000 34.000000" preserveAspectRatio="xMidYMid meet">
+                <svg id="fingerScroll" version="1.0" xmlns="http://www.w3.org/2000/svg" width="50%" viewBox="0 0 30.000000 34.000000" preserveAspectRatio="xMidYMid meet">
                     <g transform="translate(0.000000,34.000000) scale(0.100000,-0.100000)" fill="whitesmoke" stroke="none">
                         <path d="M30 293 c0 -46 -2 -52 -17 -46 -15 5 -14 1 4 -17 18 -20 24 -21 37 -10 17 14 22 35 6 25 -6 -4 -10 9 -10 32 0 21 -5 45 -10 53 -7 11 -10 0 -10 -37z" />
                         <path d="M109 316 c-17 -8 -25 -22 -27 -44 -2 -17 1 -32 6 -32 6 0 8 9 5 20 -7 26 13 50 42 50 25 0 40 -15 48 -45 4 -16 5 -15 6 6 2 42 -40 65 -80 45z" />
                         <path d="M123 292 c-6 -4 -12 -35 -15 -69 -2 -35 -7 -65 -10 -68 -3 -4 -15 -1 -27 7 -18 11 -23 10 -38 -5 -17 -17 -15 -21 53 -87 68 -66 74 -70 118 -70 36 0 52 6 71 25 22 21 25 33 25 90 0 54 -3 67 -17 71 -10 3 -25 9 -34 14 -9 5 -33 12 -53 15 -33 7 -36 10 -36 40 0 33 -17 49 -37 37z m28 -79 c1 -60 2 -64 10 -35 12 40 29 42 29 2 0 -16 5 -30 10 -30 6 0 10 9 10 20 0 11 7 20 15 20 9 0 15 -9 15 -22 1 -21 2 -22 14 -5 22 28 26 20 26 -45 0 -73 -5 -83 -44 -98 -46 -17 -78 -4 -141 60 -60 61 -67 84 -18 59 16 -7 31 -15 36 -17 4 -2 7 33 7 77 0 65 3 81 15 81 12 0 15 -15 16 -67z" />
                     </g>
                 </svg>
-                <svg id="mouseScroll" version="1.0" xmlns="http://www.w3.org/2000/svg" width="50%" height="auto" viewBox="0 0 100.000000 100.000000" preserveAspectRatio="xMidYMid meet">
+                <svg id="mouseScroll" version="1.0" xmlns="http://www.w3.org/2000/svg" width="50%" viewBox="0 0 100.000000 100.000000" preserveAspectRatio="xMidYMid meet">
                     <g transform="translate(0.000000,100.000000) scale(0.100000,-0.100000)" fill="whitesmoke" stroke="none">
                         <path d="M430 899 c-14 -5 -36 -20 -48 -32 -47 -44 -52 -66 -52 -240 0 -154 1 -166 23 -197 40 -59 71 -75 147 -75 76 0 107 16 148 75 21 31 22 43 22 200 0 157 -1 169 -22 200 -37 54 -71 73 -135 77 -32 1 -69 -2 -83 -8z m151 -42 c18 -12 39 -34 46 -47 17 -34 17 -326 0 -360 -34 -67 -141 -92 -208 -47 -54 36 -59 55 -59 227 0 104 4 163 13 180 20 40 74 70 127 70 32 0 58 -7 81 -23z" />
                         <path d="M484 777 c-3 -8 -4 -36 -2 -63 2 -36 7 -49 18 -49 20 0 22 116 3 122 -7 3 -16 -2 -19 -10z" />
@@ -169,10 +169,10 @@ if (!empty($_POST)) {
                     <h3 class="slideTitle">Une première expérience pro</h3>
                     <div class="slideContainer">
                         <div class="frontSlideContainer">
-                            <img class="frontSlide" src="../Assest/PresentationSectionImg/constructionSlide.png">
+                            <img class="frontSlide" src="../Assest/PresentationSectionImg/constructionSlide.webp" alt="Arrière-plan noir et blanc d'un chantier de construction.">
                         </div>
                         <div class="backSlideContainer">
-                            <img class="backSlide" src="../Assest/PresentationSectionImg/citySlide.png">
+                            <img class="backSlide" src="../Assest/PresentationSectionImg/citySlide.webp" alt="Arrière-plan noir et blanc d'une ville avec ses gratte-ciel.">
                         </div>
                     </div>
                     <div class="slideText flexCenter">
@@ -189,10 +189,10 @@ if (!empty($_POST)) {
                     <h3 class="slideTitle">Un voyage</h3>
                     <div class="slideContainer">
                         <div class="frontSlideContainer">
-                            <img class="frontSlide" src="../Assest/PresentationSectionImg/backpackSlide.png">
+                            <img class="frontSlide" src="../Assest/PresentationSectionImg/backpackSlide.webp" alt="Arrière-plan noir et blanc d'un homme avec un sac à dos de voyage.">
                         </div>
                         <div class="backSlideContainer">
-                            <img class="backSlide" src="../Assest/PresentationSectionImg/aroundWorldSlide.png">
+                            <img class="backSlide" src="../Assest/PresentationSectionImg/aroundWorldSlide.webp" alt="Arrière-plan noir et blanc de la terre.">
                         </div>
                     </div>
                     <div class="slideText flexCenter">
@@ -208,18 +208,18 @@ if (!empty($_POST)) {
                     <h3 class="slideTitle">Un nouvel objectif</h3>
                     <div class="slideContainer">
                         <div class="frontSlideContainer">
-                            <img class="frontSlide" src="../Assest/PresentationSectionImg/diplomeSlide.png">
+                            <img class="frontSlide" src="../Assest/PresentationSectionImg/diplomeSlide.webp" alt="Arrière-plan noir et blanc d'une remise de diplôme.">
                         </div>
                         <div class="backSlideContainer">
-                            <img class="backSlide" src="../Assest/PresentationSectionImg/officeSlide.png">
+                            <img class="backSlide" src="../Assest/PresentationSectionImg/officeSlide.webp" alt="Arrière-plan noir et blanc d'un espace de travail avec des bureaux.">
                         </div>
                     </div>
                     <div class="slideText flexCenter">
                         <p>
-                            En revenant en France j'ai décidé de changer de métier et de me lancer dans le
-                            développement web, un secteur qui m'a toujours attiré et qui est en pleine croissance. Mon diplôme
-                            de développeur web bac +2 en poche, je cherche maintenant à me spécialiser dans le back-end et
-                            recherche une alternance pour acquérir de l'expérience professionnelle.
+                            En revenant en France, j'ai décidé de changer de métier et de me lancer dans le développement web, 
+                            un secteur qui m'a toujours attiré et qui est en pleine croissance. Mon diplôme de développeur web bac +2 en poche,
+                             je cherche maintenant à me spécialiser dans le back-end et recherche une première expérience professionnelle au travers d'un CDI/CDD,
+                              une alternance, ou un stage d'immersion au sein d'une entreprise opérant dans le secteur du développement informatique.
                         </p>
                     </div>
                 </div>
@@ -247,7 +247,7 @@ if (!empty($_POST)) {
                 <div class="flipCard">
                     <div class="flipCardInner">
                         <div class="flipCardFront">
-                            <img class="workLinkImg" src="../Assest/flipCardImg/c2cImg.png" alt="">
+                            <img class="workLinkImg" src="../Assest/flipCardImg/c2cImg.webp" alt="Miniature d'image de la page d'accueil du site présenté.">
                         </div>
                         <div class="flipCardBack">
                             <p>Description du site :<br><br> Galerie d'exposition pour une photographe comportant plusieurs
